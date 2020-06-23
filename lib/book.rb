6fe6b2c40c2eaaf710d7137ba5f6b3c724f9e2cc
hahm-d <dhahm01@gmail.com>
2020-06-23 16:19:09 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_accessor :author, :page_count, :genre  #getter and #setter
+    attr_reader :title #getter
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+     
+end
 
